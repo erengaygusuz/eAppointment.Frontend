541d36d8df43ecafd80c91fbf07a8cb0f7b633d6
erengaygusuz <gaygusuzeren@gmail.com>
2024-05-01 15:14:38 +0300

    Get All Doctor Feature is Added

--- src/app/models/result.model.ts
+++ src/app/models/result.model.ts
@@ -1,5 +1,5 @@
 export class ResultModel<T>{
-    data?: T;
+    data: any;
     errorMessages?: string[];
     isSuccessfull: boolean = true;
     statusCode: number = 200;
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -13,9 +13,7 @@ export class HttpService {
   post<T>(apiUrl: string, body:any, callback: (res: ResultModel<T>) => void, errCallback?: (err: HttpErrorResponse) => void){
     this.http.post<ResultModel<T>>(`${api}/${apiUrl}`, body).subscribe({
       next: (res => {
-        if(res.data !== undefined && res.data !== null){
-          callback(res);
-        }
+        callback(res);
       }),
       error: ((err: HttpErrorResponse) => {
         if(errCallback !== undefined){
