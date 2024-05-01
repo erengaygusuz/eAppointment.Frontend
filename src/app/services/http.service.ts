ec545ca4b8f7ea881018758ba8f95100089f9ccd
erengaygusuz <gaygusuzeren@gmail.com>
2024-05-01 10:52:13 +0300

    Result Model and HttpService are Added

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ResultModel } from '../models/result.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  post<T>(apiUrl: string, body:any, callback: (res: ResultModel<T>) => void, errCallback: (err: HttpErrorResponse) => void){
+    this.http.post<ResultModel<T>>(`${apiUrl}`, body).subscribe({
+      next: (res => {
+        callback(res);
+      }),
+      error: ((err: HttpErrorResponse) => {
+        if(errCallback !== undefined){
+          errCallback(err);
+        }
+      })
+    })
+  }
+}
